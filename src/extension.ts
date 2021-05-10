3178df777b75415ac4c5d59b5dceb5ef1475fdb0
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2021-05-10 17:23:24 +0200

    Simulate moving cursor

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('vscode-undo.helloWorld', async () => {
 		let editor = vscode.window.activeTextEditor
 		if(editor) {
-			let selection = editor.selection
+			var selection = editor.selection
 
 			//First insert
 			await editor.edit(editBuilder => {
@@ -35,36 +35,61 @@ export function activate(context: vscode.ExtensionContext) {
 			 })
 
 			await sleep(500)
+			await vscode.commands.executeCommand("cursorMove",
+                {
+                    to: "down", by:'wrappedLine', value:1
+                });
+			selection = editor.selection
 
 			//Second insert
 			await editor.edit(editBuilder => {
 				editBuilder.replace(new vscode.Position(1,0), "\n")
 			}, {
 				// Make a new undo stop
-				undoStopBefore: true,
+				undoStopBefore: false, //true, - doesn't matter
 				// Keep the stop open so we merge with the subsequent insertion or
 				// deletion
 				undoStopAfter: false
-			})
+			}).then((r) => {
+				//Do it only if the selection has changed
+				if (editor && editor.selection !== selection) {
+					//Make sure the active selection stays at the same place
+					editor.selection = selection
+				}
+				return r;
+			 })
 
 			await sleep(500)
+			await vscode.commands.executeCommand("cursorMove",
+			{
+				to: "down", by:'wrappedLine', value:1
+			});
+			selection = editor.selection
 
 			//Third insert
 			await editor.edit(editBuilder => {
 				editBuilder.replace(new vscode.Position(2,0), "\n")
 			}, {
 				// Make a new undo stop
-				undoStopBefore: true,
+				undoStopBefore: false, //true, - doesn't matter
 				// Keep the stop open so we merge with the subsequent insertion or
 				// deletion
 				undoStopAfter: false
-			})
+			}).then((r) => {
+				//Do it only if the selection has changed
+				if (editor && editor.selection !== selection) {
+					//Make sure the active selection stays at the same place
+					editor.selection = selection
+				}
+				return r;
+			 })
 
 			await sleep(500)
 
+
 			//Delete
 			await editor.edit(editBuilder => {
-				editBuilder.delete(new vscode.Range(new vscode.Position(2,0), new vscode.Position(7,1)))
+				editBuilder.delete(new vscode.Range(new vscode.Position(2,0), new vscode.Position(6,1)))
 			}, {
                 // Merge with the insertion stop
                 undoStopBefore: false,
